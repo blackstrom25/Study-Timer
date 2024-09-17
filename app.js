936880d5df8fc1f2ac9f5bde81a9ac1ba5b76718
blackstrom25 <hartejsandhu1125@gmail.com>
2024-09-17 19:14:02 +0530

    Updated styles and added some functional changes

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const timerDisplay = document.querySelector(".main-timer");
 const userInputSection = document.querySelector(".user-Input");
 const userInputMins = document.querySelector(".user-Mins");
 const userInputSecs = document.querySelector(".user-Secs");
+const errorMsg = document.querySelector(".error-msg");
 // const userInputBtn = document.querySelector(".inpBtn");
 
 let myMin = 0;
 let mySec = 0;
-disableTimer = false;
+let disableTimer = false;
 
 function timerStart(){
 
@@ -41,23 +42,37 @@ function resetTimer(){
 }
 
 startBtn.addEventListener("click", () =>{
-    disableTimer = false;
-    myMin = userInputMins.value == '' ? 0 : userInputMins.value;
-    mySec = userInputSecs.value == '' ? 0 : userInputSecs.value;
-    userInputMins.value = '';
-    userInputSecs.value = '';
-    setInitial()
-    timerStart();
-    resetBtn.classList.remove("hide")
-    // startBtn.classList.add("hide")
-    userInputSection.classList.add("hide")
-    resetBtn.classList.remove("hide")
+
+    if (userInputMins.value == '' || userInputSecs.value == ''){
+        errorMsg.classList.remove("hide");
+        errorMsg.innerText = "Enter some value";
+        userInputMins.value = '';
+        userInputSecs.value = '';
+    }
+    else if (userInputMins.value < 0 || userInputSecs.value > 59 || userInputSecs.value < 0){
+        errorMsg.classList.remove("hide");
+        errorMsg.innerText = "Enter valid value";
+        userInputMins.value = '';
+        userInputSecs.value = '';
+    }
+    else{
+        errorMsg.innerText = "";
+        errorMsg.classList.add("hide");
+        myMin = userInputMins.value;
+        mySec = userInputSecs.value;
+        disableTimer = false;
+        userInputMins.value = '';
+        userInputSecs.value = '';
+        setInitial()
+        timerStart();
+        resetBtn.classList.remove("hide")
+        userInputSection.classList.add("hide")
+    }
 });
 
 resetBtn.addEventListener("click", () => {
     disableTimer = true;
     resetTimer()
     resetBtn.classList.add("hide")
-    // startBtn.classList.remove("hide")
     userInputSection.classList.remove("hide")
 })
